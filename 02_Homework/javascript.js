b294790404734e6ad3d54949107a7fb75aebbb6f
Anton <soshnikov-1988@inbox.ru>
2018-10-06 02:32:49 +0300

    Это наш первый второй коментарий

---
+++ 02_Homework/javascript.js
@@ -0,0 +1,45 @@
+'use strict';
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+for (let i = 0; i < 2; i++) {
+    let answer = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        answer1 = prompt("Во сколько обойдется?", "");
+    if ( (typeof(a))=== 'string' && (typeof(answer)) != null && (typeof(answer1)) != null
+        && answer != '' && answer1 != '' && answer.length<50) {
+        console.log("done");
+        appData.expenses[answer] = answer1;    
+    } else {
+
+    }    
+};
+//  while (i < 2) {
+//    let answer = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         answer1 = +prompt("Во сколько обойдется?", "");  
+//         console.log("done");
+//         i++;
+//  };
+// let i = 0;
+// do {    let answer = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//             answer1 = +prompt("Во сколько обойдется?", ""); 
+//             console.log("done");
+//             appData.expenses[answer] = answer1;
+//             i++;
+// }   while (i<2); 
+
+
+
+console.log(money);
+console.log(time);    
+console.log(appData.expenses);
+    
+
+alert(money/30);
